0789f99e20bfa38bb934b05dae89e2170785ea88
BigAngryPanda <mrtrollpanda@gmail.com>
2022-09-06 23:59:56 +0300

    Rustfmt

--- src/memory.rs
+++ src/memory.rs
@@ -4,13 +4,13 @@
 
 use ash::vk;
 
-use crate::{hw, dev, surface, swapchain, graphics};
 use crate::on_error_ret;
+use crate::{dev, graphics, hw, surface, swapchain};
 
-use std::ptr;
-use std::fmt;
-use std::error::Error;
 use core::ffi::c_void;
+use std::error::Error;
+use std::fmt;
+use std::ptr;
 
 // TODO mb rewrite it with separate flags?
 
@@ -95,14 +95,18 @@ impl<'a> Memory<'a> {
         );
 
         let requirements: vk::MemoryRequirements = unsafe {
-            mem_cfg.device.device().get_buffer_memory_requirements(buffer)
+            mem_cfg
+                .device
+                .device()
+                .get_buffer_memory_requirements(buffer)
         };
 
-        let memory_filter = |m : &hw::MemoryDescription| -> Option<u32> {
-            if ((requirements.memory_type_bits >> m.index()) & 1) == 1 && m.is_compatible(mem_cfg.properties) {
+        let memory_filter = |m: &hw::MemoryDescription| -> Option<u32> {
+            if ((requirements.memory_type_bits >> m.index()) & 1) == 1
+                && m.is_compatible(mem_cfg.properties)
+            {
                 Some(m.index())
-            }
-            else {
+            } else {
                 None
             }
         };
@@ -119,30 +123,43 @@ impl<'a> Memory<'a> {
             memory_type_index: mem_index,
         };
 
-        let dev_memory:vk::DeviceMemory = on_error_ret!(
+        let dev_memory: vk::DeviceMemory = on_error_ret!(
             unsafe { mem_cfg.device.device().allocate_memory(&memory_info, None) },
             MemoryError::DeviceMemory
         );
 
         // Without coherency we have to manually synchronize memory between host and device
-        if !mem_cfg.properties.contains(vk::MemoryPropertyFlags::HOST_COHERENT)
-            && mem_cfg.properties.contains(vk::MemoryPropertyFlags::HOST_VISIBLE) {
+        if !mem_cfg
+            .properties
+            .contains(vk::MemoryPropertyFlags::HOST_COHERENT)
+            && mem_cfg
+                .properties
+                .contains(vk::MemoryPropertyFlags::HOST_VISIBLE)
+        {
             let mem_range = vk::MappedMemoryRange {
                 s_type: vk::StructureType::MAPPED_MEMORY_RANGE,
                 p_next: ptr::null(),
                 memory: dev_memory,
                 offset: 0,
-                size: vk::WHOLE_SIZE
+                size: vk::WHOLE_SIZE,
             };
 
             unsafe {
                 on_error_ret!(
-                    mem_cfg.device.device().map_memory(dev_memory, 0, mem_cfg.size, vk::MemoryMapFlags::empty()),
+                    mem_cfg.device.device().map_memory(
+                        dev_memory,
+                        0,
+                        mem_cfg.size,
+                        vk::MemoryMapFlags::empty()
+                    ),
                     MemoryError::MapAccess
                 );
 
                 on_error_ret!(
-                    mem_cfg.device.device().flush_mapped_memory_ranges(&[mem_range]),
+                    mem_cfg
+                        .device
+                        .device()
+                        .flush_mapped_memory_ranges(&[mem_range]),
                     MemoryError::Flush
                 );
 
@@ -151,19 +168,22 @@ impl<'a> Memory<'a> {
         }
 
         on_error_ret!(
-            unsafe { mem_cfg.device.device().bind_buffer_memory(buffer, dev_memory, 0) },
+            unsafe {
+                mem_cfg
+                    .device
+                    .device()
+                    .bind_buffer_memory(buffer, dev_memory, 0)
+            },
             MemoryError::Bind
         );
 
-        Ok(
-			Memory {
-				i_device: mem_cfg.device,
-				i_device_memory: dev_memory,
-				i_buffer: buffer,
-				i_size: mem_cfg.size,
-				i_flags: mem_cfg.properties,
-			}
-		)
+        Ok(Memory {
+            i_device: mem_cfg.device,
+            i_device_memory: dev_memory,
+            i_buffer: buffer,
+            i_size: mem_cfg.size,
+            i_flags: mem_cfg.properties,
+        })
     }
 
     /// Performs action on mutable memory
@@ -172,30 +192,41 @@ impl<'a> Memory<'a> {
     /// [vkFlushMappedMemoryRanges](https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/vkFlushMappedMemoryRanges.html)
     ///
     /// In other words makes host memory changes available to device
-	pub fn write<F>(&self, f: &mut F) -> Result<(), MemoryError>
-    where F: FnMut(&mut [u8])
+    pub fn write<F>(&self, f: &mut F) -> Result<(), MemoryError>
+    where
+        F: FnMut(&mut [u8]),
     {
         let data: *mut c_void = on_error_ret!(
             unsafe {
-                self.i_device.device().map_memory(self.i_device_memory, 0, self.i_size, vk::MemoryMapFlags::empty())
+                self.i_device.device().map_memory(
+                    self.i_device_memory,
+                    0,
+                    self.i_size,
+                    vk::MemoryMapFlags::empty(),
+                )
             },
             MemoryError::MapAccess
         );
 
-        f(unsafe {std::slice::from_raw_parts_mut(data as *mut u8, self.i_size as usize)});
+        f(unsafe { std::slice::from_raw_parts_mut(data as *mut u8, self.i_size as usize) });
 
-        if !self.i_flags.contains(vk::MemoryPropertyFlags::HOST_COHERENT) {
+        if !self
+            .i_flags
+            .contains(vk::MemoryPropertyFlags::HOST_COHERENT)
+        {
             let mem_range = vk::MappedMemoryRange {
                 s_type: vk::StructureType::MAPPED_MEMORY_RANGE,
                 p_next: ptr::null(),
                 memory: self.i_device_memory,
                 offset: 0,
-                size: vk::WHOLE_SIZE
+                size: vk::WHOLE_SIZE,
             };
 
             on_error_ret!(
                 unsafe {
-                    self.i_device.device().flush_mapped_memory_ranges(&[mem_range])
+                    self.i_device
+                        .device()
+                        .flush_mapped_memory_ranges(&[mem_range])
                 },
                 MemoryError::Flush
             );
@@ -214,20 +245,24 @@ impl<'a> Memory<'a> {
     /// I.e. makes device memory changes available to host (compare with [Memory::write()] method)
     ///
     /// Note: on failure return same error [MemoryError::Flush]
-    pub fn read(&self) -> Result<&[u8], MemoryError>
-    {
-        if !self.i_flags.contains(vk::MemoryPropertyFlags::HOST_COHERENT) {
+    pub fn read(&self) -> Result<&[u8], MemoryError> {
+        if !self
+            .i_flags
+            .contains(vk::MemoryPropertyFlags::HOST_COHERENT)
+        {
             let mem_range = vk::MappedMemoryRange {
                 s_type: vk::StructureType::MAPPED_MEMORY_RANGE,
                 p_next: ptr::null(),
                 memory: self.i_device_memory,
                 offset: 0,
-                size: vk::WHOLE_SIZE
+                size: vk::WHOLE_SIZE,
             };
 
             on_error_ret!(
                 unsafe {
-                    self.i_device.device().invalidate_mapped_memory_ranges(&[mem_range])
+                    self.i_device
+                        .device()
+                        .invalidate_mapped_memory_ranges(&[mem_range])
                 },
                 MemoryError::Flush
             );
@@ -235,12 +270,18 @@ impl<'a> Memory<'a> {
 
         let data: *mut c_void = on_error_ret!(
             unsafe {
-                self.i_device.device().map_memory(self.i_device_memory, 0, self.i_size, vk::MemoryMapFlags::empty())
+                self.i_device.device().map_memory(
+                    self.i_device_memory,
+                    0,
+                    self.i_size,
+                    vk::MemoryMapFlags::empty(),
+                )
             },
             MemoryError::MapAccess
         );
 
-        let result: &[u8] = unsafe {std::slice::from_raw_parts_mut(data as *mut u8, self.i_size as usize)};
+        let result: &[u8] =
+            unsafe { std::slice::from_raw_parts_mut(data as *mut u8, self.i_size as usize) };
 
         unsafe { self.i_device.device().unmap_memory(self.i_device_memory) };
 
@@ -262,7 +303,9 @@ impl<'a> Drop for Memory<'a> {
     fn drop(&mut self) {
         unsafe {
             self.i_device.device().destroy_buffer(self.i_buffer, None);
-            self.i_device.device().free_memory(self.i_device_memory, None);
+            self.i_device
+                .device()
+                .free_memory(self.i_device_memory, None);
         };
     }
 }
@@ -284,8 +327,12 @@ pub struct Image<'a> {
 
 impl<'a> Image<'a> {
     #[doc(hidden)]
-    fn new(device: &'a dev::Device, img: vk::Image, img_format: vk::Format) -> Result<Image<'a>, ImageError> {
-        let image_info:vk::ImageViewCreateInfo = vk::ImageViewCreateInfo {
+    fn new(
+        device: &'a dev::Device,
+        img: vk::Image,
+        img_format: vk::Format,
+    ) -> Result<Image<'a>, ImageError> {
+        let image_info: vk::ImageViewCreateInfo = vk::ImageViewCreateInfo {
             s_type: vk::StructureType::IMAGE_VIEW_CREATE_INFO,
             p_next: ptr::null(),
             flags: vk::ImageViewCreateFlags::empty(),
@@ -312,12 +359,10 @@ impl<'a> Image<'a> {
             ImageError::ImageView
         );
 
-        Ok(
-            Image {
-                i_dev: device,
-                i_image_view: img_view
-            }
-        )
+        Ok(Image {
+            i_dev: device,
+            i_image_view: img_view,
+        })
     }
 
     pub fn view(&self) -> vk::ImageView {
@@ -327,24 +372,33 @@ impl<'a> Image<'a> {
 
 impl<'a> Drop for Image<'a> {
     fn drop(&mut self) {
-        unsafe { self.i_dev.device().destroy_image_view(self.i_image_view, None) };
+        unsafe {
+            self.i_dev
+                .device()
+                .destroy_image_view(self.i_image_view, None)
+        };
     }
 }
 
 pub struct ImageListType<'a> {
     pub device: &'a dev::Device,
-    pub swapchain: &'a swapchain::Swapchain
+    pub swapchain: &'a swapchain::Swapchain,
 }
 
 /// Collection of [`Images`](Image)
-pub struct ImageList<'a>(Vec::<Image<'a>>);
+pub struct ImageList<'a>(Vec<Image<'a>>);
 
 impl<'a> ImageList<'a> {
     /// Retrieves [image handlers](Image) from [`Swapchain`](crate::swapchain::Swapchain)
-    pub fn from_swapchain<'b>(swp_type: &'b ImageListType<'a>) -> Result<ImageList<'a>, ImageError> {
+    pub fn from_swapchain<'b>(
+        swp_type: &'b ImageListType<'a>,
+    ) -> Result<ImageList<'a>, ImageError> {
         let swapchain_images = on_error_ret!(
             unsafe {
-                swp_type.swapchain.loader().get_swapchain_images(swp_type.swapchain.swapchain())
+                swp_type
+                    .swapchain
+                    .loader()
+                    .get_swapchain_images(swp_type.swapchain.swapchain())
             },
             ImageError::GetImages
         );
@@ -398,9 +452,12 @@ pub struct Framebuffer<'a> {
 
 impl<'a> Framebuffer<'a> {
     #[doc(hidden)]
-    fn new(dev: &'a dev::Device, img: vk::ImageView, extent: vk::Extent2D, rp: vk::RenderPass)
-        -> Result<Framebuffer<'a>, FramebufferError>
-    {
+    fn new(
+        dev: &'a dev::Device,
+        img: vk::ImageView,
+        extent: vk::Extent2D,
+        rp: vk::RenderPass,
+    ) -> Result<Framebuffer<'a>, FramebufferError> {
         let create_info = vk::FramebufferCreateInfo {
             s_type: vk::StructureType::FRAMEBUFFER_CREATE_INFO,
             p_next: ptr::null(),
@@ -418,12 +475,10 @@ impl<'a> Framebuffer<'a> {
             FramebufferError::Framebuffer
         );
 
-        Ok(
-            Framebuffer {
-                i_dev: dev,
-                i_frame: framebuffer,
-            }
-        )
+        Ok(Framebuffer {
+            i_dev: dev,
+            i_frame: framebuffer,
+        })
     }
 }
 
@@ -443,18 +498,22 @@ pub struct FramebufferType<'a> {
 }
 
 /// Collection of [`Framebuffers`](Framebuffer)
-pub struct FramebufferList<'a>(Vec::<Framebuffer<'a>>);
+pub struct FramebufferList<'a>(Vec<Framebuffer<'a>>);
 
 impl<'a> FramebufferList<'a> {
     pub fn new<'b>(cfg: &'b FramebufferType<'a>) -> Result<FramebufferList<'a>, FramebufferError> {
         let mut list: Vec<Framebuffer<'a>> = Vec::new();
 
         for img in cfg.images.images() {
-            list.push(
-                on_error_ret!(
-                    Framebuffer::new(cfg.device, img.view(), cfg.extent, cfg.render_pass.render_pass()),
-                    FramebufferError::Framebuffer)
-                );
+            list.push(on_error_ret!(
+                Framebuffer::new(
+                    cfg.device,
+                    img.view(),
+                    cfg.extent,
+                    cfg.render_pass.render_pass()
+                ),
+                FramebufferError::Framebuffer
+            ));
         }
 
         Ok(FramebufferList(list))
