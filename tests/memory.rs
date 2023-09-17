07d03580d2cecbdf58f3eba5d45791d124daf2f7
BigAngryPanda <mrtrollpanda@gmail.com>
2023-09-17 16:09:24 +0200

    Remove zero_allocation test

--- tests/memory.rs
+++ tests/memory.rs
@@ -59,51 +59,6 @@ mod memory {
         assert!(memory::Memory::allocate(&device, &mem_cfg).is_ok());
     }
 
-    //#[test]
-    fn zero_allocation() {
-        let lib_type = libvk::InstanceType {
-            debug_layer: Some(layers::DebugLayer::default()),
-            extensions: &[extensions::DEBUG_EXT_NAME],
-            ..libvk::InstanceType::default()
-        };
-
-        let lib = libvk::Instance::new(&lib_type).expect("Failed to load library");
-        let hw_list = hw::Description::poll(&lib, None).expect("Failed to list hardware");
-
-        let (hw_dev, queue, _) = hw_list
-            .find_first(
-                hw::HWDevice::is_dedicated_gpu,
-                hw::QueueFamilyDescription::is_compute,
-                |_| true
-            )
-            .expect("Failed to find suitable hardware device");
-
-        let dev_type = dev::DeviceCfg {
-            lib: &lib,
-            hw: hw_dev,
-            extensions: &[],
-            allocator: None,
-        };
-
-        let device = dev::Device::new(&dev_type).expect("Failed to create device");
-
-        let compute_memory = memory::BufferCfg {
-            size: 0,
-            usage: memory::STORAGE,
-            queue_families: &[queue.index()],
-            simultaneous_access: false,
-            count: 1
-        };
-
-        let mem_cfg = memory::MemoryCfg {
-            properties: hw::MemoryProperty::HOST_VISIBLE,
-            filter: &hw::any,
-            buffers: &[&compute_memory]
-        };
-
-        assert!(memory::Memory::allocate(&device, &mem_cfg).is_err());
-    }
-
     #[test]
     fn multiple_buffers() {
         let lib_type = libvk::InstanceType {
